bf9a8d07c16552790a478102483000f63fe70061
matcastaneda <matiascmondaca@gmail.com>
2024-10-26 14:48:59 -0300

    feat: update log help message with available templates

--- src/utils/log-help-message.ts
+++ src/utils/log-help-message.ts
@@ -1,14 +1,27 @@
+import { availableTemplates } from '@/utils/available-templates';
 import { colorText } from '@/utils/color-text';
-import { getTemplateNames } from '@/utils/get-template-config';
 
 /**
  * Log the help message for the `init` command.
  * @returns The help message.
  */
 export function logHelpMessage() {
-  const templates = getTemplateNames();
   return `
 Templates:
-  ${templates?.map((template) => colorText(`- ${template}`, 'cyan')).join('\n  ')}
+  ${
+    !availableTemplates?.length
+      ? colorText(
+          'Templates are currently under construction. Please check back soon!',
+          'yellow'
+        )
+      : availableTemplates
+          ?.map((template) => {
+            const templateText = `- ${template.title}`;
+            return template.disabled
+              ? `${colorText(templateText, 'gray')} ${colorText('(soon)', 'yellow')}`
+              : colorText(templateText, 'cyan');
+          })
+          .join('\n  ')
+  }
 `;
 }
