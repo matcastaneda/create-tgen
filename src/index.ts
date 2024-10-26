53cf72a1f6f4dbaa387882d038bf5dbe2478b257
matcastaneda <matiascmondaca@gmail.com>
2024-10-26 14:39:50 -0300

    feat: add function to get version of tgen

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import { Command } from 'commander';
 
 import { init } from '@/commands/init';
+import { getPackageInfo } from '@/utils/get-project-info';
 import { logHelpMessage } from '@/utils/log-help-message';
 
 const handleSigTerm = () => process.exit(0);
@@ -10,10 +11,16 @@ process.on('SIGINT', handleSigTerm);
 process.on('SIGTERM', handleSigTerm);
 
 async function main() {
+  const packageInfo = await getPackageInfo();
+
   const program = new Command()
     .name('create-tgen')
     .description('A CLI tool to generate TypeScript/JavaScript projects')
-    .version('0.0.1', '-v, --version', 'output the current version')
+    .version(
+      packageInfo?.version || '0.1.0',
+      '-v, --version',
+      'output the current version'
+    )
     .addHelpText('after', logHelpMessage());
 
   program.addCommand(init);
--- src/utils/get-project-info.ts
+++ src/utils/get-project-info.ts
@@ -1,3 +1,10 @@
+import fs from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import type { PackageJson } from 'type-fest';
+
+import { logger } from '@/utils/logger';
+
 export function getUserAgentInfo(userAgent: string | undefined) {
   if (!userAgent) return undefined;
   const pkgSpec = userAgent.split(' ')[0];
@@ -6,4 +13,17 @@ export function getUserAgentInfo(userAgent: string | undefined) {
     name: pkgSpecArr?.[0],
     version: pkgSpecArr?.[1],
   };
+}
+
+export async function getPackageInfo() {
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+  const packageJsonPath = path.join(__dirname, '..', 'package.json');
+
+  try {
+    const packageJson = await fs.readFile(packageJsonPath, 'utf-8');
+    return JSON.parse(packageJson) as PackageJson;
+  } catch (error) {
+    logger.error('  Failed to read package.json');
+    return null;
+  }
 }
